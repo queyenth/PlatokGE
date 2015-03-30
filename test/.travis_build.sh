dc49435f2c41236b4e731ec6c653c265873a6509
Queyenth <queyenth@gmail.com>
2015-03-31 00:26:27 +0600

    Travis build script

---
+++ test/.travis_build.sh
@@ -0,0 +1,8 @@
+echo "Compiling unit tests..."
+g++ Main.cc -std=c++11 -lgtest -lpthread -o unit_test
+echo "Running unit tests..."
+./unit_test
+result=$?
+rm -r unit_test
+echo "Unit tests completed : $result"
+exit $result
